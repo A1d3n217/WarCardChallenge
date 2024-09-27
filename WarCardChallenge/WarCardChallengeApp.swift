ce4cd6367623df8315c6180adb089d84372ce501
Aiden Wood <aidenwwood@gmail.com>
2024-09-27 09:57:01 -0600

    Initial Commit

---
+++ WarCardChallenge/WarCardChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardChallengeApp.swift
+//  WarCardChallenge
+//
+//  Created by Wood, Aiden - Student on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WarCardChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
